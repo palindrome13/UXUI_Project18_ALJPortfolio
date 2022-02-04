704cd7f72190d9b00f42099f94f749e115152440
palindrome13 <94757075+palindrome13@users.noreply.github.com>
2022-02-03 20:50:39 -0500

    final updates on html css for gwu

---
+++ index.js
@@ -0,0 +1,4 @@
+// var img = document.getElementById('eatme');
+// document.getElementById('eatme').onmouseover = function(){
+    //manipulate the image source here.
+    // img.src = img.src.replace(/\.EatMe_Overlay_img.png/, 'EatMe_Hover_imgcolor.png');
